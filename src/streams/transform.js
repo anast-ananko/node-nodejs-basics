c65579b7b34974ad3e3746cdd34bbbe2d827a474
Nastassia Ananka <anast.ananko@gmail.com>
2023-06-15 23:02:51 +0200

    feat: complete streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, _, callback) {     
+      const reversedChunk = chunk.toString().split('').reverse().join('');
+    
+      console.log(reversedChunk);
+    
+      callback();
+    }
+  });
+
+  await pipeline(process.stdin, reverseStream, process.stdout);
 };
 
 await transform();
