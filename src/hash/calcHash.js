9600c878fc3a83ff27918ee58128f3992dc93709
Nastassia Ananka <anast.ananko@gmail.com>
2023-06-15 16:09:11 +0200

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { readFile } from "fs/promises";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  try {
+    const content = await readFile(filePath, { flag: 'r' });
+    hash.update(content);
+
+    const hashContent = hash.digest('hex');
+    console.log(hashContent);
+  } catch (error) {
+		throw new Error('Operation failed');
+  }
 };
 
 await calculateHash();
