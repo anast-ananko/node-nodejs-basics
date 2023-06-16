556e86a2a8ebf017c4be2e7f1b30dab02d6a47b2
Nastassia Ananka <anast.ananko@gmail.com>
2023-06-16 17:16:27 +0200

    feat: complete worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = join(__dirname, 'worker.js');
+const startNumber = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+
+  const createWorkerPromise = async (workerData) => {
+    return new Promise(function (resolve, reject) {
+      const worker = new Worker(workerPath, { workerData });
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));      
+      worker.on('error', () => reject({ status: 'error', data: null }));
+    }).catch(() => {
+      return { status: 'error', data: null };
+    });
+  }
+
+  const numCores = cpus().length;
+  const workerPromises = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const dataToSend = startNumber + i;
+    const workerPromise = createWorkerPromise(dataToSend);
+    workerPromises.push(workerPromise);    
+  }
+
+  try {
+    const results = await Promise.all(workerPromises);
+    console.log(results);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await performCalculations();
